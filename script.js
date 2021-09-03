247a1c889c11d0873be52ef3ebba19e05054cb05
ichbhawna <rathibhawna18@gmail.com>
2021-09-03 23:44:51 +0530

    new update

---
+++ script.js
@@ -0,0 +1,8 @@
+// const arrows  = document.querySelectorAll('.arrow');
+// const movielist = document.querySelectorAll('.movie-list');
+
+// arrows.forEach((arrow,i)=>{
+//   arrow.addEventListener("cick", () => {
+//     movielistlist[i].style.transform = `transform(${
+//       movielist[i].computedStyleMap().get("transform")[0].x.value = 270})`;
+//   })
